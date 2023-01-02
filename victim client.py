7c961022127257c50cb058cd88196c6c6e9e3152
mrvs0 <mrvs025@gmail.com>
2023-01-02 18:16:55 +0200

    branch initialization creating main adding server ip to func

--- victim client.py
+++ victim client.py
@@ -1,6 +1,12 @@
 import functions
 
-ip_to_connect = ('127.0.0.1', 4200)
-server_ip = ('127.0.0.1', 42069)
 
-functions.connect_as_victim(ip_for_victims=ip_to_connect)
+def main():
+    ip_to_connect = ('127.0.0.1', 4200)
+    server_ip = ('127.0.0.1', 42069)
+
+    functions.connect_as_victim(ip_for_victims=ip_to_connect, ip_of_server=server_ip)
+
+
+if __name__ == '__main__':
+    main
